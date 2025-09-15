7f165311f79bedfba0b68641b5fade72884d42c8
sbx_user1051 <sbx_user1051@169.254.47.109>
2025-09-15 11:56:44 +0000

    Mon Sep 15 11:56:44 UTC 2025

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -5,5 +5,6 @@ class HelloSolution:
     def hello(self, friend_name:str)->str:
         if not isinstance(friend_name,str):
             raise TypeError(f"'friend_name' must be of type {type(friend_name)}")
-        return f"Hello {friend_name}"
+        return f"Hello, World!"
+
 

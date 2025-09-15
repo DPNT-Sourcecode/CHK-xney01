40589fd1e838089628235aac4d6ca5a6b05dd030
sbx_user1051 <sbx_user1051@169.254.81.61>
2025-09-15 12:31:59 +0000

    Mon Sep 15 12:31:59 UTC 2025

--- lib/solutions/CHK/checkout_tests.py
+++ lib/solutions/CHK/checkout_tests.py
@@ -1,6 +1,9 @@
 from lib.solutions.CHK.checkout_solution import CheckoutSolution
 import pytest
 
+# Not doing extensive tests 
 class TestCheckout:
     def test_single_items(self):
-        assert CheckoutSolution.checkout("A") == 50
+        checkout = CheckoutSolution()
+        assert checkout.checkout("A") == 50
+        assert checkout.checkout("B") == 30

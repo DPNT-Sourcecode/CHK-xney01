ecb898115d3fadd2288dee4c27fb1540c88d96d5
sbx_user1051 <sbx_user1051@169.254.47.109>
2025-09-15 11:41:41 +0000

    Mon Sep 15 11:41:41 UTC 2025

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -2,30 +2,29 @@
 class SumSolution:
     
     def compute(self, x:int, y:int)->int:
-        """_summary_
+        """Adds two integers (strictly) between 0 and 100
 
         Args:
-            x (int): _description_
-            y (int): _description_
+            x (int): First integer (0<x<100)
+            y (int): Second integer (0<y<100)
 
         Raises:
             TypeError: _description_
-            TypeError: _description_
-            ValueError: _description_
             ValueError: _description_
 
         Returns:
-            int: _description_
+            int: The sum of x and y
         """
         if not isinstance(x,int):
-            raise TypeError(f"'x' must be of type ")
+            raise TypeError(f"'x' must be of type {type(x)}")
         if not isinstance(y,int):
             raise TypeError(f"")
-        if not (0<=x<=100):
+        if not (0<x<100):
             raise ValueError(f"'x' must be between 0 and 100, got {x}")
-        if not (0<=y<=100):
+        if not (0<y<100):
             raise ValueError(f"'y' must be between 0 and 100, got {y}")
         return x + y
+
 
 
 

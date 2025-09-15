a582ca7c3e225e69c9ffb5db8149cdeaa8e3d725
sbx_user1051 <sbx_user1051@169.254.81.61>
2025-09-15 13:27:40 +0000

    Mon Sep 15 13:27:40 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -13,7 +13,7 @@ class CheckoutSolution:
         "H":{"unit_price":10,"special_offers":[(10,80),(5,45)]},
         "I":{"unit_price":35,"special_offers":[]},
         "J":{"unit_price":60,"special_offers":[]},
-        "K":{"unit_price":80,"special_offers":[(2,150)]},
+        "K":{"unit_price":70,"special_offers":[(2,150)]},
         "L":{"unit_price":90,"special_offers":[]},
         "M":{"unit_price":15,"special_offers":[]},
         "N":{"unit_price":40,"special_offers":[]},
@@ -21,14 +21,14 @@ class CheckoutSolution:
         "P":{"unit_price":50,"special_offers":[(5,200)]},
         "Q":{"unit_price":30,"special_offers":[(3,80)]},
         "R":{"unit_price":50,"special_offers":[]},
-        "S":{"unit_price":30,"special_offers":[]},
+        "S":{"unit_price":20,"special_offers":[]},
         "T":{"unit_price":20,"special_offers":[]},
         "U":{"unit_price":40,"special_offers":[(4,120)]},
         "V":{"unit_price":50,"special_offers":[(3,130),(2,90)]},
         "W":{"unit_price":20,"special_offers":[]},
-        "X":{"unit_price":90,"special_offers":[]},
-        "Y":{"unit_price":10,"special_offers":[]},
-        "Z":{"unit_price":50,"special_offers":[]},
+        "X":{"unit_price":17,"special_offers":[]},
+        "Y":{"unit_price":20,"special_offers":[]},
+        "Z":{"unit_price":21,"special_offers":[]},
         }
     
     EXTRA_OFFERS = {
@@ -40,36 +40,36 @@ class CheckoutSolution:
         """
 
         Our price table and offers:
-        +------+-------+------------------------+
-        | Item | Price | Special offers         |
-        +------+-------+------------------------+
-        | A    | 50    | 3A for 130, 5A for 200 |
-        | B    | 30    | 2B for 45              |
-        | C    | 20    |                        |
-        | D    | 15    |                        |
-        | E    | 40    | 2E get one B free      |
-        | F    | 10    | 2F get one F free      |
-        | G    | 20    |                        |
-        | H    | 10    | 5H for 45, 10H for 80  |
-        | I    | 35    |                        |
-        | J    | 60    |                        |
-        | K    | 80    | 2K for 150             |
-        | L    | 90    |                        |
-        | M    | 15    |                        |
-        | N    | 40    | 3N get one M free      |
-        | O    | 10    |                        |
-        | P    | 50    | 5P for 200             |
-        | Q    | 30    | 3Q for 80              |
-        | R    | 50    | 3R get one Q free      |
-        | S    | 30    |                        |
-        | T    | 20    |                        |
-        | U    | 40    | 3U get one U free      |
-        | V    | 50    | 2V for 90, 3V for 130  |
-        | W    | 20    |                        |
-        | X    | 90    |                        |
-        | Y    | 10    |                        |
-        | Z    | 50    |                        |
-        +------+-------+------------------------+
+        +------+-------+---------------------------------+
+        | Item | Price | Special offers                  |
+        +------+-------+---------------------------------+
+        | A    | 50    | 3A for 130, 5A for 200          |
+        | B    | 30    | 2B for 45                       |
+        | C    | 20    |                                 |
+        | D    | 15    |                                 |
+        | E    | 40    | 2E get one B free               |
+        | F    | 10    | 2F get one F free               |
+        | G    | 20    |                                 |
+        | H    | 10    | 5H for 45, 10H for 80           |
+        | I    | 35    |                                 |
+        | J    | 60    |                                 |
+        | K    | 70    | 2K for 120                      |
+        | L    | 90    |                                 |
+        | M    | 15    |                                 |
+        | N    | 40    | 3N get one M free               |
+        | O    | 10    |                                 |
+        | P    | 50    | 5P for 200                      |
+        | Q    | 30    | 3Q for 80                       |
+        | R    | 50    | 3R get one Q free               |
+        | S    | 20    | buy any 3 of (S,T,X,Y,Z) for 45 |
+        | T    | 20    | buy any 3 of (S,T,X,Y,Z) for 45 |
+        | U    | 40    | 3U get one U free               |
+        | V    | 50    | 2V for 90, 3V for 130           |
+        | W    | 20    |                                 |
+        | X    | 17    | buy any 3 of (S,T,X,Y,Z) for 45 |
+        | Y    | 20    | buy any 3 of (S,T,X,Y,Z) for 45 |
+        | Z    | 21    | buy any 3 of (S,T,X,Y,Z) for 45 |
+        +------+-------+---------------------------------+
 
         Notes:
          - For any illegal input return -1
@@ -110,6 +110,7 @@ class CheckoutSolution:
             total += count*unit_price
 
         return total
+
 
 
 

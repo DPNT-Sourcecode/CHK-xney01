dd12291d4dfbd0fd8e66fcc92b4249984dd65877
sbx_user1051 <sbx_user1051@169.254.47.109>
2025-09-15 11:47:47 +0000

    Mon Sep 15 11:47:47 UTC 2025

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -2,11 +2,11 @@
 class SumSolution:
     
     def compute(self, x:int, y:int)->int:
-        """Adds two integers (strictly) between 0 and 100
+        """Adds two integers between 0 and 100 (inclusive)
 
         Args:
-            x (int): First integer (0<x<100)
-            y (int): Second integer (0<y<100)
+            x (int): First integer (0<=x<=100)
+            y (int): Second integer (0<=y<=100)
 
         Raises:
             TypeError: If inputs aren't integers.
@@ -19,11 +19,12 @@ class SumSolution:
             raise TypeError(f"'x' must be of type {type(x)}")
         if not isinstance(y,int):
             raise TypeError(f"'y' must be of type {type(y)}")
-        if not (0<x<100):
+        if not (0<=x<=100):
             raise ValueError(f"'x' must be between 0 and 100, got {x}")
-        if not (0<y<100):
+        if not (0<=y<=100):
             raise ValueError(f"'y' must be between 0 and 100, got {y}")
         return x + y
+
 
 
 

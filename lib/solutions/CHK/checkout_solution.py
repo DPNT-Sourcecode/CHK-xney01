1753d1e2ea4d08c9be666c9638b8a41db65cade2
sbx_user1051 <sbx_user1051@169.254.81.61>
2025-09-15 13:10:44 +0000

    Mon Sep 15 13:10:44 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -14,6 +14,26 @@ class CheckoutSolution:
         | D    | 15    |                        |
         | E    | 40    | 2E get one B free      |
         | F    | 10    | 2F get one F free      |
+        | G    | 20    |                        |
+        | H    | 10    | 5H for 45, 10H for 80  |
+        | I    | 35    |                        |
+        | J    | 60    |                        |
+        | K    | 80    | 2K for 150             |
+        | L    | 90    |                        |
+        | M    | 15    |                        |
+        | N    | 40    | 3N get one M free      |
+        | O    | 10    |                        |
+        | P    | 50    | 5P for 200             |
+        | Q    | 30    | 3Q for 80              |
+        | R    | 50    | 3R get one Q free      |
+        | S    | 30    |                        |
+        | T    | 20    |                        |
+        | U    | 40    | 3U get one U free      |
+        | V    | 50    | 2V for 90, 3V for 130  |
+        | W    | 20    |                        |
+        | X    | 90    |                        |
+        | Y    | 10    |                        |
+        | Z    | 50    |                        |
         +------+-------+------------------------+
 
         Notes:
@@ -32,6 +52,21 @@ class CheckoutSolution:
             "C":{"unit_price":20,"special_offers":[]},
             "D":{"unit_price":15,"special_offers":[]},
             "E":{"unit_price":40,"special_offers":[]},
+            "F":{"unit_price":10,"special_offers":[(3,20)]},
+            "G":{"unit_price":10,"special_offers":[(3,20)]},
+            "H":{"unit_price":10,"special_offers":[(3,20)]},
+            "I":{"unit_price":10,"special_offers":[(3,20)]},
+            "J":{"unit_price":10,"special_offers":[(3,20)]},
+            "K":{"unit_price":10,"special_offers":[(3,20)]},
+            "L":{"unit_price":10,"special_offers":[(3,20)]},
+            "M":{"unit_price":10,"special_offers":[(3,20)]},
+            "F":{"unit_price":10,"special_offers":[(3,20)]},
+            "F":{"unit_price":10,"special_offers":[(3,20)]},
+            "F":{"unit_price":10,"special_offers":[(3,20)]},
+            "F":{"unit_price":10,"special_offers":[(3,20)]},
+            "F":{"unit_price":10,"special_offers":[(3,20)]},
+            "F":{"unit_price":10,"special_offers":[(3,20)]},
+            "F":{"unit_price":10,"special_offers":[(3,20)]},
             "F":{"unit_price":10,"special_offers":[(3,20)]},
         }
 
@@ -64,6 +99,3 @@ class CheckoutSolution:
             total += count*unit_price
 
         return total
-
-
-

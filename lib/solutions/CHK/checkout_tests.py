832173cf4c84712b273e829727409b2668bbc28a
sbx_user1051 <sbx_user1051@169.254.81.61>
2025-09-15 12:59:13 +0000

    Mon Sep 15 12:59:13 UTC 2025

--- lib/solutions/CHK/checkout_tests.py
+++ lib/solutions/CHK/checkout_tests.py
@@ -11,7 +11,22 @@ class TestCheckout:
     def test_many_items(self):
         checkout = CheckoutSolution()
         assert checkout.checkout("ABACA") == 180
+        assert checkout.checkout("AAAAAAAA") == 330
 
+    def test_extra_offers(self): 
+        checkout = CheckoutSolution()
+        assert checkout.checkout("EE") == 80
+        assert checkout.checkout("EEB") == 80
+        assert checkout.checkout("EEBB") == 110
+
+    def test_large_mixed_basket(self):
+        checkout = CheckoutSolution()
+        assert checkout.checkout("ABCDEEAA") == 245
+
+    def test_illegal_inputs(self):
+        checkout = CheckoutSolution()
+        assert checkout.checkout("X") == -1
+        assert checkout.checkout(123) == -1
 
     def test_empty_basket(self):
         checkout = CheckoutSolution()

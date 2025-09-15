125bb6369a0531b1f0680a7ba080a9ebe2f5cc7d
sbx_user1051 <sbx_user1051@169.254.47.109>
2025-09-15 11:38:41 +0000

    Mon Sep 15 11:38:41 UTC 2025

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -2,10 +2,30 @@
 class SumSolution:
     
     def compute(self, x:int, y:int)->int:
+        """_summary_
+
+        Args:
+            x (int): _description_
+            y (int): _description_
+
+        Raises:
+            TypeError: _description_
+            TypeError: _description_
+            ValueError: _description_
+            ValueError: _description_
+
+        Returns:
+            int: _description_
+        """
+        if not isinstance(x,int):
+            raise TypeError(f"'x' must be of type ")
+        if not isinstance(y,int):
+            raise TypeError(f"")
         if not (0<=x<=100):
-            raise ValueError(f"")
-        if not (0<=y<=100)
-        return x+y
-        raise NotImplementedError()
+            raise ValueError(f"'x' must be between 0 and 100, got {x}")
+        if not (0<=y<=100):
+            raise ValueError(f"'y' must be between 0 and 100, got {y}")
+        return x + y
+
 
 
